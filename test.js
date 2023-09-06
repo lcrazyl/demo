b7e64b80fde8fda1023240540e4b9804f5bd2228
李佳奇 <swiftslee@gmail.com>
2023-09-06 22:52:34 +0800

    test

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
 /*
  * @Author: 20224787
  * @Date: 2023-09-06 22:46:47
- * @LastEditTime: 2023-09-06 22:48:22
+ * @LastEditTime: 2023-09-06 22:52:30
  * @Content: 
  * @Modify: 
  */
 console.log('test')
-console.log(1)
