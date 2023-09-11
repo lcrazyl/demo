913b293f93cfbc0fe466003c669bbce9383ab561
李佳奇 <swiftslee@gmail.com>
2023-09-11 22:13:27 +0800

    test

--- 1.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: 20224787
- * @Date: 2023-09-06 23:46:33
- * @LastEditTime: 2023-09-10 19:32:21
- * @Content: 
- * @Modify: 
- */
-let name = 10
-console.log('11111111')
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>1111</div>
+  </div>
+</template>
+<script setup>
+    const d = ref(1)
+     function name(params) {
+      const {} = params
+    }
+</script>
+<style scoped>
+
+</style>
